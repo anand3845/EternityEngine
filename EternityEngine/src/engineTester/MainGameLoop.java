fa4784eaf08115e76e4a681aeab21c98fe94372f
anand <anand@192.168.2.7>
2017-08-15 16:16:05 +0530

    Added Features:
    1. Fog
    2. Multitexture using Blend Map

--- EternityEngine/src/engineTester/MainGameLoop.java
+++ EternityEngine/src/engineTester/MainGameLoop.java
@@ -1,6 +1,9 @@
 package engineTester;
 
-import models.RawModel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import models.TexturedModel;
 
 import org.lwjgl.opengl.Display;
@@ -8,10 +11,12 @@ import org.lwjgl.util.vector.Vector3f;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
+import terrains.Terrain;
 import textures.ModelTexture;
+import textures.TerrainTexture;
+import textures.TerrainTexturePack;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
@@ -22,35 +27,54 @@ public class MainGameLoop {
 
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		StaticShader shader = new StaticShader();
-		Renderer renderer = new Renderer(shader);
 		
 		
-		RawModel model = OBJLoader.loadObjModel("stall", loader);
 		
-		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("stallTexture")));
-		ModelTexture texture = staticModel.getTexture();
-		texture.setShineDamper(8);
-		texture.setReflectivity(1f);
+		TexturedModel tree = new TexturedModel(OBJLoader.loadObjModel("tree", loader),new ModelTexture(loader.loadTexture("tree")));
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1);
-		Light light = new Light(new Vector3f(3000,2000,3000),new Vector3f(1,1,1));
+		TexturedModel grass = new TexturedModel(OBJLoader.loadObjModel("grassModel", loader),new ModelTexture(loader.loadTexture("grassTexture")));
+		grass.getTexture().setHasTransparency(true);
+		grass.getTexture().setUseFakeLighting(true);
+		TexturedModel fern = new TexturedModel(OBJLoader.loadObjModel("fern", loader),new ModelTexture(loader.loadTexture("fern")));
+		fern.getTexture().setHasTransparency(true);
+		List<Entity> entities = new ArrayList<Entity>();
+		Random random = new Random();
+		for(int i=0;i<500;i++){
+			entities.add(new Entity(tree, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,3));
+			entities.add(new Entity(grass, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,1));
+			entities.add(new Entity(fern, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,0.6f));
+		}
 		
-		Camera camera = new Camera();
+		Light light = new Light(new Vector3f(20000,20000,2000),new Vector3f(1,1,1));
+		
+		TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grassy2"));
+		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("mud"));
+		TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("grassFlowers"));
+		TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("path"));
+		TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
+		
+		TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
+		 		
+		
+		Terrain terrain = new Terrain(0,-1,loader, texturePack, blendMap);
+		Terrain terrain2 = new Terrain(0,-1,loader, texturePack, blendMap);
+		
+		Camera camera = new Camera();	
+		MasterRenderer renderer = new MasterRenderer();
 		
 		while(!Display.isCloseRequested()){
-			entity.increaseRotation(0, 1, 0);
 			camera.move();
-			renderer.prepare();
-			shader.start();
-			shader.loadLight(light);
-			shader.loadViewMatrix(camera);
-			renderer.render(entity,shader);
-			shader.stop();
+			
+			renderer.processTerrain(terrain);
+			renderer.processTerrain(terrain2);
+			for(Entity entity:entities){
+				renderer.processEntity(entity);
+			}
+			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
 		}
 
-		shader.cleanUp();
+		renderer.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
--- EternityEngine/src/entities/Camera.java
+++ EternityEngine/src/entities/Camera.java
@@ -1,33 +1,65 @@
 package entities;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
 	
 	private Vector3f position = new Vector3f(0,5,0);
-	private float pitch;
+	private float pitch = 10;
 	private float yaw ;
 	private float roll;
-	
+		
 	public Camera(){}
 	
 	public void move(){
-		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-			position.z-=0.2f;
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-			position.x+=0.2f;
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
-			position.x-=0.2f;
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)){
-			position.y+=0.2f;
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)){
-			position.y-=0.2f;
-		}
+		
+		float arg_yaw = Mouse.getDX() ;
+        yaw += arg_yaw/10 ;
+        float arg_roll = Mouse.getDY() ;
+        pitch += -(arg_roll/10) ;
+        Mouse.setGrabbed(true);
+        
+	                if (Keyboard.isKeyDown(Keyboard.KEY_W)) 
+            {
+                float toZ = ((float)Math.sin( Math.toRadians(yaw+90))) ;
+                float toX = ((float)Math.cos( Math.toRadians(yaw+90))) ;
+                position.x -= toX;
+                position.z -= toZ;
+                    
+	                }
+            if (Keyboard.isKeyDown(Keyboard.KEY_S)) 
+            {
+                float toZ = ((float)Math.sin( Math.toRadians(yaw+90))) ;
+                float toX = ((float)Math.cos( Math.toRadians(yaw+90))) ;
+                position.x += toX;
+                position.z += toZ;
+	                }
+
+	                if (Keyboard.isKeyDown(Keyboard.KEY_D)) 
+            {
+                float toZ = ((float)Math.sin( Math.toRadians(yaw))) ;
+                float toX = ((float)Math.cos( Math.toRadians(yaw))) ;
+                position.x += toX;
+                position.z += toZ;
+	                }
+
+	                if (Keyboard.isKeyDown(Keyboard.KEY_A)) 
+            {
+                float toZ = ((float)Math.sin( Math.toRadians(yaw))) ;
+                float toX = ((float)Math.cos( Math.toRadians(yaw))) ;
+                position.x -= toX;
+                position.z -= toZ;
+	                }
+            if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) 
+            {
+                position.y += 0.2f;
+	                }
+            if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) 
+            {
+                position.y -= 0.2f;
+            }
 	}
 
 	public Vector3f getPosition() {
